d6f16503f826dae6d63bdcec075bb3cb6c6d09a0
Gautam <gaudeep2k1@gmail.com>
2021-08-24 19:21:45 +0530

    second commit

--- lib/StartUpModel.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:college/push_notification.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-
-class StartUpViewModel extends BaseModel{
-
-
-}
