3bb2a88c494f770fb91f50964b2172682fc2c136
gautam0901-maker <gaudeep2k1@gmail.com>
2022-01-31 12:25:31 +0530

    changes login screen

--- lib/Google_Sign_up_widget.dart
+++ lib/Google_Sign_up_widget.dart
@@ -5,29 +5,33 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class GoogleSignInButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
-    padding: EdgeInsets.all(4),
-    child: OutlineButton.icon(
-      label: Text('Countinue With Google',
-       style: TextStyle(
-         fontSize: 20,
-         fontWeight: FontWeight.bold,
-       ),),
-      shape: StadiumBorder(),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical:8 ),
-      highlightColor: Colors.black,
-      borderSide: BorderSide(color: Colors.black),
-      textColor: Colors.blue,
-      icon:Icon(
-        Icons.account_circle_outlined,
-        color: Colors.red,
-      ),
-      onPressed: (){
-          final provider = Provider.of<GoogleSignInProvider>(context, listen : false);
-          provider.login();
-      },
-    ),
-  );
+        padding: EdgeInsets.all(4),
+        child: OutlineButton.icon(
+          label: Text(
+            'Countinue With Google',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          shape: StadiumBorder(),
+          padding: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+          highlightColor: Colors.black,
+          borderSide: BorderSide(color: Colors.black),
+          textColor: Colors.blue,
+          icon: Icon(
+            Icons.account_circle_outlined,
+            color: Colors.blue,
+          ),
+          onPressed: () {
+            final provider =
+                Provider.of<GoogleSignInProvider>(context, listen: false);
+            provider.login();
+          },
+        ),
+      );
 }
--- lib/sign_up_wigdget.dart
+++ lib/sign_up_wigdget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:college/Google_Sign_up_widget.dart';
 import 'package:college/google_sign_in.dart';
 import 'package:flutter/gestures.dart';
@@ -9,43 +10,47 @@ class SignUpWidget extends StatelessWidget {
   Widget build(BuildContext context) => buildSignUp();
 
   Widget buildSignUp() => Column(
-    children: [
-      Spacer(),
-    Row(
-      children: [
-        Container(
-  decoration: new BoxDecoration(
-
-    image: new DecorationImage(image: new AssetImage('images/prince1PSVPEC.jpg'),
-
-    ),
-  ),
-          margin : EdgeInsets.symmetric(horizontal :20),
-          width : 175,
-        child: Text(
-           'Welcome To   Prince College ',
-   style : TextStyle(
-          color : Colors.black,
-  fontSize : 42,
-   fontWeight : FontWeight.bold,
-          )
-          )
+        children: [
+          Spacer(),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                width: 175,
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TyperAnimatedText('Welcome To   Price College',
+                        textStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 42,
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ],
+                ),
+              ),
+              new Container(
+                child: new Image.asset(
+                  'images/prince1PSVPEC.jpg',
+                  height: 60.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
           ),
-      ],
-    ),
-
-
-      Spacer(),
-      GoogleSignInButtonWidget(),
-      SizedBox(height: 20,),
-      Text('Login to countinue',
-     style: TextStyle(fontSize: 16,
-     fontWeight: FontWeight.bold,
-       color: Colors.black,
-     ),
-      ),
-      Spacer(),
-    ],
-  );
+          Spacer(),
+          GoogleSignInButtonWidget(),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Login to countinue',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black26,
+            ),
+          ),
+          Spacer(),
+        ],
+      );
 }
-
